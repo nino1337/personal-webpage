cf9147c199d2b87476376d14e9441ea85ace86dd
Nino Dornemann <nino.dornemann@urbanenergy.de>
2022-05-20 08:52:30 +0200

    disable client side translation to enable ssr content

--- next-app/pages/_app.js
+++ next-app/pages/_app.js
@@ -1,26 +1,25 @@
-import 'intersection-observer';
-
 import { Global } from '@emotion/core';
 import { ThemeProvider } from 'emotion-theming';
+import 'intersection-observer';
 import { useRouter } from 'next/router';
 import propTypes from 'prop-types';
-import React, { useEffect, useState } from 'react';
-
+import React, { useEffect } from 'react';
 import LocalizationContext from '../localization/context';
-import getTranslationsFromPage from '../localization/getTranslationsFromPage';
 import globalStyle from '../styles/globalStyles';
 import theme from '../styles/theme';
+
 function MyApp({ Component, pageProps }) {
-  const [localizedData, setLocalizedData] = useState(null);
+  // const [localizedData, setLocalizedData] = useState(null);
   const router = useRouter();
 
   useEffect(() => {
     if (router.asPath !== '/') {
       router.push('/');
     } else {
+      /* disable client slide translation for now, to enable ssr content - todo: move translation to server side (next-i18n-translation)
       const navigatorLanguage = navigator.language || navigator.userLanguage;
-
-      setLocalizedData(getTranslationsFromPage(navigatorLanguage, pageProps));
+      setLocalizedData(getTranslationsFromPage(navigatorLanguage, pageProps)); 
+      */
     }
     // keep app state in sync with current page
   }, [router.asPath]);
@@ -28,11 +27,9 @@ function MyApp({ Component, pageProps }) {
   return (
     <ThemeProvider theme={theme}>
       <Global styles={globalStyle} />
-      {localizedData && (
-        <LocalizationContext.Provider value={localizedData}>
-          <Component {...pageProps} />
-        </LocalizationContext.Provider>
-      )}
+      <LocalizationContext.Provider value={pageProps}>
+        <Component {...pageProps} />
+      </LocalizationContext.Provider>
     </ThemeProvider>
   );
 }
